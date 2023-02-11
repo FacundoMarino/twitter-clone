a0c7b4511f68fbe0775424946d8345380bc6eba3
Facundo Marino <38021147+FacundoMarino@users.noreply.github.com>
2023-02-11 18:51:20 -0300

    FIX: secret next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  secret: process.env.SECRET,
 
   callbacks: {
     async session({ session, token }) {
@@ -20,7 +21,5 @@ export const authOptions = {
       return session;
     },
   },
-
-  secret: process.env.SECRET,
 };
 export default NextAuth(authOptions);
